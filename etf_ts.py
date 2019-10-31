Added report of total assets and fixed average model.

etf_ts.py
@@ -76,6 +76,7 @@ flags.DEFINE_float(
     'mix_applying_period', 0.25,
     'For the mix strategy, the amount of years (fractional) the strategy is applied after optimized. '
     'Defines how often one will have to re-invest.')
+
 flags.DEFINE_integer(
     'max_days', None,
     'If set, look at most at the latest given number of days. Otherwise look at everything.')
@@ -117,7 +118,7 @@ def main(argv):
 
     # Print out gains for each symbol.
     # Header of all outputs.
-    print('Symbol,Gain,Adjusted Gain,Initial,Final,Description')
+    print('Symbol,Gain,Adjusted Gain,Initial,Final,Total Assets,Description')
     if 'average' in FLAGS.stats:
         average(symbols, mask, fields)
     # if 'greedy' in FLAGS.stats:
@@ -125,7 +126,7 @@ def main(argv):
     if 'mix' in FLAGS.stats:
         mix_previous_period(symbols, mask, fields, all_serials)
     if 'per_asset' in FLAGS.stats:
-        per_asset_gains(symbols, mask, fields)
+        per_asset_gains(dmgr, symbols, mask, fields)
     if 'selection' in FLAGS.stats:
         assets_selection(symbols, mask, fields, all_serials)
 
@@ -143,10 +144,11 @@ def _get_symbols(selection: Optional[List[Text]]) -> List[Text]:
                     filtered.append(symbol)
             symbols = filtered
         else:
+            symbols = selection
             logging.info(f'Symbols: {symbols}')
     return symbols
 
-def per_asset_gains(symbols: List[Text], mask: tf.Tensor, fields: Dict[Text, tf.Tensor]) -> None:
+def per_asset_gains(dmgr: data_manager.DataManager, symbols: List[Text], mask: tf.Tensor, fields: Dict[Text, tf.Tensor]) -> None:
     """Print basic assets data (CSV) and per assets gains and adjusted gains."""
     # Find opening and closing values of each asset.
     open_t = tf.transpose(fields['Open'])
@@ -179,10 +181,13 @@ def per_asset_gains(symbols: List[Text], mask: tf.Tensor, fields: Dict[Text, tf.
             description = config_ib.SYMBOL_TO_INFO[symbol]['description']
             currency = config_ib.SYMBOL_TO_INFO[symbol]['currency']
         adjusted_gain = total_adjusted_gain[symbol_idx]
+        total_assets = 0
+        if symbol in dmgr.total_assets and dmgr.total_assets[symbol] is not None:
+            total_assets = dmgr.total_assets[symbol]
 
         initial_value = initial_values[symbol_idx]
         final_value = final_values[symbol_idx]
-        print(f'{symbol},{gain:.4f},{adjusted_gain:.4f},{initial_value:.2f},{final_value:.2f},{description}')
+        print(f'{symbol},{gain:.4f},{adjusted_gain:.4f},{initial_value:.2f},{final_value:.2f},{total_assets:.2g},{description}')
 
 
 def average(symbols: List[Text], mask: tf.Tensor, fields: Dict[Text, tf.Tensor]) -> None:
@@ -190,12 +195,16 @@ def average(symbols: List[Text], mask: tf.Tensor, fields: Dict[Text, tf.Tensor])
     log_gains = tf.where(mask, log_gains, tf.zeros_like(
         log_gains, dtype=tf.float32))
 
-    partial_gains: List[float] = []
-    partial_adjusted_gains: List[float] = []
+    all_gains: tf.Tensor = tf.constant(0.0)
+    all_adjusted_gains: tf.Tensor = tf.constant(0.0)
     logits = tf.zeros(dtype=tf.float32, shape=tf.shape(log_gains)[-1])
 
+    is_last = False
     for ii in range(config.REPORT_PERIOD_YEARS):
         start_idx = -(ii + 1) * config.YEARLY_PERIOD_IN_SERIAL
+        if -start_idx >= log_gains.shape[0]:
+            start_idx = -log_gains.shape[0]
+            is_last = True
         end_idx = -ii * config.YEARLY_PERIOD_IN_SERIAL
         if ii == 0:
             mask_year = mask[start_idx:, :]
@@ -210,17 +219,19 @@ def average(symbols: List[Text], mask: tf.Tensor, fields: Dict[Text, tf.Tensor])
 
         # Find gains for the year.
         mix_gain, adjusted_mix_gain = optimizations.mix_gain(
-            logits, symbols_used, log_gains_year, FLAGS.loss_cost)
-        partial_gains.append(mix_gain.numpy())
-        partial_adjusted_gains.append(adjusted_mix_gain.numpy())
+            logits, symbols_used, log_gains_year, FLAGS.loss_cost,
+            FLAGS.gain_power)
+        all_gains += tf.math.log(mix_gain)
+        all_adjusted_gains += tf.math.log(adjusted_mix_gain)
 
-    # print(f'partial_gains={partial_gains}')
-    # print(f'partial_adjusted_gains={partial_adjusted_gains}')
-    average = np.mean(np.array(partial_gains))
-    average = 100.0 * (average - 1.0)
-    adjusted_average = np.mean(np.array(partial_adjusted_gains))
-    adjusted_average = 100.0 * (adjusted_average - 1.0)
-    print(f'_Average (last {config.REPORT_PERIOD_YEARS} years),{average:.4f},{adjusted_average:.4f},,,Gains (p.a.), readjusted every year.')
+        if is_last:
+            break
+
+    cycles = -start_idx
+    years = float(cycles) / config.YEARLY_PERIOD_IN_SERIAL
+    all_gains, all_adjusted_gains = _report_pct_all_gains(all_gains, all_adjusted_gains, cycles)
+    print(f'_average (last {years:.1f} years),' +
+          f'{all_gains:.2f},{all_adjusted_gains:.2f},,,,Gains (p.a.), readjusted every year.')
 
 
 def mix_previous_period(symbols: List[Text], mask: tf.Tensor, fields: Dict[Text, tf.Tensor], all_serials: List[int]) -> None:
@@ -301,21 +312,24 @@ def mix_previous_period(symbols: List[Text], mask: tf.Tensor, fields: Dict[Text,
         adjusted_mix_gain = 100.0 * (adjusted_mix_gain - 1.0)
         logging.info(f'  gain={mix_gain:.2f}%, adjusted_gain={adjusted_mix_gain:.2f}% (both annualized)')
 
-    all_gains = tf.math.exp(all_gains)
-    print(f'all_gains={all_gains:.4f}, cycles={-apply_start}')
-    all_gains = optimizations.annualized_gain(all_gains, -apply_start)
-    all_gains = 100.0 * (all_gains - 1.0)
-
-    all_adjusted_gains = tf.math.exp(all_adjusted_gains)
-    print(f'all_adjusted_gains={all_adjusted_gains:.4f}, cycles={-apply_start}')
-    all_adjusted_gains = optimizations.annualized_gain(
-        all_adjusted_gains, -apply_start)
-    all_adjusted_gains = 100.0 * (all_adjusted_gains - 1.0)
-
+    all_gains, all_adjusted_gains = _report_pct_all_gains(all_gains, all_adjusted_gains, -apply_start)
     print(f'_mix (last {config.REPORT_PERIOD_YEARS} years),' +
           f'{all_gains:.2f},{all_adjusted_gains:.2f},,,' +
           f'Trained with preceding {FLAGS.mix_training_period} year(s) ' +
           f'and then applied for {FLAGS.mix_applying_period} year(s).')
+
+
+def _report_pct_all_gains(all_gains: tf.Tensor, all_adjusted_gains: tf.Tensor, cycles: int) -> Tuple[tf.Tensor, tf.Tensor]:
+    all_gains = tf.math.exp(all_gains)
+    print(f'all_gains={all_gains:.4f}, cycles={cycles}')
+    all_gains = optimizations.annualized_gain(all_gains, cycles)
+    all_gains = 100.0 * (all_gains - 1.0)
+
+    all_adjusted_gains = tf.math.exp(all_adjusted_gains)
+    print(f'all_adjusted_gains={all_adjusted_gains:.4f}, cycles={cycles}')
+    all_adjusted_gains = optimizations.annualized_gain(all_adjusted_gains, cycles)
+    all_adjusted_gains = 100.0 * (all_adjusted_gains - 1.0)
+    return all_gains, all_adjusted_gains
 
 
 def _normalize_selection(symbols: List[Text], logits: tf.Tensor, logits_mask: tf.Tensor) -> List[Tuple[float, Text]]: